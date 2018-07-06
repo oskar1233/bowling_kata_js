b2e2b4a624889dd8201f5bf1227bc0fb3ac9ecba
Oskar Laska <oskar.laska@republic-m.com>
2018-07-06 12:09:52 +0200

    Logic & tests.

--- app.js
+++ app.js
@@ -1,17 +1,91 @@
 class Game {
 
     constructor() {
-        this.rolls = [];
+        // Is current roll even
+        this._even = false
+
+        // Played frames count
+        this._frames = 0
+
+        // Previous roll
+        this._prev = 0
+
+        // Current score
+        this._score = 0
+
+        // How many steps further count double
+        this._bonus = 0
+
+        // Should we count bonus two times; for double strikes
+        this._doubleBonus = 0
     }
 
+    /**
+     * Add roll.
+     *
+     * Changes points accordingly to the roll's value.
+     *
+     * @param {integer} pins Value of current roll
+     *
+     * @return undefined
+     *
+     * @throws {Error} Thrown if `pins` are not valid value regarding current state 
+     */
     roll(pins) {
-        this.rolls.push(pins);
+        const _err = this._validateRoll(pins)
+        if(_err) throw new Error(_err)
+
+        // Add points
+        if(this._bonus > 0) {
+            if(this._doubleBonus) {
+                this._score += 3*pins
+                this._doubleBonus = false
+            } else {
+                this._score += 2*pins
+            }
+            --this._bonus
+        } else {
+            this._score += pins
+        }
+
+        // Check for bonuses
+        if(!this._even && pins == 10) { // Strike
+            if(this._bonus > 0) this._doubleBonus = true
+
+            this._bonus = 2
+            this._even  = true // Act as like current roll is even
+        } else if(this._even && pins + this._prev >= 10) { // Spare
+            this._bonus = 1
+        }
+
+        // Proceed to next roll with valid values
+        if(this._even) ++this._frames
+        this._prev = pins
+        this._even = !this._even
     }
 
     score() {
-        return this.rolls.reduce((accumulator, currentValue) => accumulator + currentValue);
+        return this._score
+    }
+
+    /**
+     * Validate roll.
+     *
+     * Throws exception in case of error.
+     *
+     * @param {integer} curr Roll to be verified
+     *
+     * @return {null|string} Error message or null
+     */
+    _validateRoll(curr) {
+        if(curr > 10) return "Invalid input; too many points."
+
+        if(this._frames > 10) return "Invalid input; too many rolls."
+        if(this._frames == 10 && this._bonus == 0) return "Invalid input; too many rolls."
+
+        return null
     }
 
 }
 
-module.exports = Game;
+module.exports = Game
--- app.spec.js
+++ app.spec.js
@@ -1,17 +1,171 @@
-const Game = require('./app.js');
+const Game = require('./app.js')
+
+/**
+ * Helper function; rolls X times with value of 1.
+ *
+ * @param {count}   integer How many times should we roll
+ * @param {game}    Game    Game to roll in
+ */
+function rollTimes(count, game) {
+    for(let i = 0; i < count; ++i) {
+        game.roll(1)
+    }
+}
 
 describe('Test Bowling Game', () => {
 
-    let game;
+    let game
 
     beforeEach(() => {
-        game = new Game();
-    });
+        game = new Game()
+    })
+
+    test('throws exception if too many points in roll', () => {
+        expect(() => {
+            game.roll(11)
+        }).toThrow()
+    })
+
+    test('throws exception if too many rolls', () => {
+        rollTimes(20, game)
+
+        expect(() => {
+            game.roll(1)
+        }).toThrow()
+    })
+
+    test('throws exception if too many rolls [strike at the end]', () => {
+        rollTimes(18, game)
+
+        game.roll(10)
+
+        game.roll(1)
+        game.roll(1)
+
+        expect(() => {
+            game.roll(1)
+        }).toThrow()
+    })
+
+    test('throws exception if too many rolls [spare at the end]', () => {
+        rollTimes(18, game)
+
+        game.roll(5)
+        game.roll(5)
+
+        game.roll(1)
+
+        expect(() => {
+            game.roll(1)
+        }).toThrow()
+    })
+
+    test('throws exception if too many rolls [spare + strike at the end]', () => {
+        rollTimes(18, game)
+
+        game.roll(5)
+        game.roll(5)
+
+        game.roll(10)
+
+        expect(() => {
+            game.roll(1)
+        }).toThrow()
+    })
+
+    test('throws exception if too many rolls [strike + spare at the end]', () => {
+        rollTimes(18, game)
+
+        game.roll(10)
+
+        game.roll(5)
+        game.roll(5)
+
+        expect(() => {
+            game.roll(1)
+        }).toThrow()
+    })
+    test('throws exception if too many rolls [double strike at the end]', () => {
+        rollTimes(18, game)
+
+        game.roll(10)
+        game.roll(10)
+
+        expect(() => {
+            game.roll(1)
+        }).toThrow()
+    })
 
     test('score returns sum of rolls', () => {
-        game.roll(5);
-        game.roll(5);
-        expect(game.score()).toEqual(10);
-    });
+        game.roll(5)
+        game.roll(5)
+        expect(game.score()).toEqual(10)
+    })
 
-});
+    test('counts strike properly', () => {
+        game.roll(10)
+        game.roll(5)
+        game.roll(2)
+        expect(game.score()).toEqual(24)
+    })
+
+    test('counts spare properly', () => {
+        game.roll(5)
+        game.roll(5)
+        game.roll(2)
+        game.roll(2)
+        expect(game.score()).toEqual(16)
+    })
+
+    // Integration tests
+    test('sample frames', () => {
+        game.roll(5)
+        game.roll(5)
+
+        expect(game.score()).toEqual(10)
+
+        game.roll(5)
+        game.roll(5)
+
+        expect(game.score()).toEqual(25)
+
+        game.roll(10)
+
+        expect(game.score()).toEqual(45)
+
+        game.roll(9)
+        game.roll(9)
+
+        expect(game.score()).toEqual(81)
+
+        game.roll(1)
+        game.roll(1)
+
+        expect(game.score()).toEqual(84)
+
+        game.roll(10)
+
+        expect(game.score()).toEqual(94)
+
+        game.roll(10)
+
+        expect(game.score()).toEqual(114)
+
+        game.roll(4)
+        game.roll(2)
+
+        expect(game.score()).toEqual(130)
+    })
+
+    test('perfect game', () => {
+        for(let i = 0; i < 11; ++i) {
+            game.roll(10)
+        }
+
+        expect(game.score()).toEqual(300)
+
+        expect(() => {
+            game.roll(10)
+        }).toThrow()
+    })
+})
